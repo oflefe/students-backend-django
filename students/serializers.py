6d8a22c1c4060bb4e842e3f97abffb57c0319e4a
EfeOfluoglu <oflefe@gmail.com>
2022-10-28 13:53:44 +0300

    emptied master

--- students/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Students
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Students
-        fields = ("pk", "name", "email", "document", "phone", "registrationDate")
